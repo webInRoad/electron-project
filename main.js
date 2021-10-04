7ccd390911522b4b99ee065b4e06609b22313e0a
webInRoad <1040233545@qq.com>
2021-10-04 17:16:41 +0800

    项目初始化

---
+++ main.js
@@ -0,0 +1,18 @@
+const { app, BrowserWindow } = require('electron')
+
+// 当 app 启动之后执行窗口创建等操作
+app.whenReady().then(() => {
+	const mainWindow = new BrowserWindow({
+		width: 600,
+		height: 400
+	})
+	// 在当前窗口中加载指定界面让它显示具体的内容
+	mainWindow.loadFile('index.html')
+	mainWindow.on('close', () => {
+		console.info('close')
+	})
+})
+app.on('window-all-closed', () => {
+	console.info('window-all-closed')
+	app.quit()
+})
