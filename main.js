c8cdbb2702eb37aa41699d53fab678be6a72555f
webInRoad <1040233545@qq.com>
2021-10-04 20:09:53 +0800

    窗口尺寸的设置，比如相对屏幕的窗口位置，最大最小尺寸，是否允许修改尺寸

--- main.js
+++ main.js
@@ -2,11 +2,22 @@ const { app, BrowserWindow } = require('electron')
 
 function createWindow() {
 	let mainWindow = new BrowserWindow({
+		x: 100,
+		y: 100, //设置窗口显示的位置，相对于当前屏幕的左上角
+		show: false, //默认情况下创建一个窗口对象之后就会显示，设置为 false 就不会显示，就不会有白屏问题
 		width: 600,
-		height: 400
+		height: 400,
+		maxHeight: 600,
+		maxWidth: 1000,
+		minHeight: 200,
+		minWidth: 300, //可以通过 min max 来设置当前应用窗口的最大和最小尺寸
+		resizable: false // 是否允许缩放应用的窗口大小
 	})
 	// 在当前窗口中加载指定界面让它显示具体的内容
 	mainWindow.loadFile('index.html')
+	mainWindow.on('ready-to-show', () => {
+		mainWindow.show()
+	})
 	mainWindow.on('close', () => {
 		console.log('8888---->当窗口关闭时触发，此时应删除窗口引用')
 		mainWindow = null
@@ -26,15 +37,15 @@ app.on('window-all-closed', () => {
 	console.log('4444---->所有窗口都被关闭时触发')
 	app.quit() // 当定义 window-all-closed 事件时，要手动添加该代码
 })
-app.on('before-quit', () => {
-	console.log('5555---->在关闭窗口之前触发')
-})
-app.on('will-quit', () => {
-	console.log('6666---->在窗口关闭并且应用退出时触发')
-})
-app.on('quit', () => {
-	console.log('7777---->在所有窗口被关闭时触发')
-})
+// app.on('before-quit', () => {
+// 	console.log('5555---->在关闭窗口之前触发')
+// })
+// app.on('will-quit', () => {
+// 	console.log('6666---->在窗口关闭并且应用退出时触发')
+// })
+// app.on('quit', () => {
+// 	console.log('7777---->在所有窗口被关闭时触发')
+// })
 // // 当 app 启动之后执行窗口创建等操作
 // app.whenReady().then(() => {
 // 	const mainWindow = new BrowserWindow({
