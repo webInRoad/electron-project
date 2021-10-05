2c771273aa63fa91438b252011575411496e277c
webInRoad <1040233545@qq.com>
2021-10-05 18:05:42 +0800

    模态窗口 modal:true

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
-const { BrowserWindow } = require('@electron/remote')
+const remote = require('@electron/remote')
 document.addEventListener('DOMContentLoaded', () => {
 	let obtn = document.getElementById('btn')
 	obtn.addEventListener('click', () => {
 		console.info(111)
 		// 创建新窗口
-		let indexMain = new BrowserWindow({
+		let indexMain = new remote.BrowserWindow({
+			parent: remote.getCurrentWindow(),
 			width: 200,
-			height: 200
+			height: 200,
+			modal: true
 		})
 		indexMain.loadFile('modal.html')
 		indexMain.on('close', () => {
